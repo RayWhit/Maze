bd8b6ea6dcf8edcf17fd9c5120e5510da9521c74
rayks <rayks100@gmail.com>
2023-01-12 17:34:09 +0100

    Menu added

--- Maze.cpp
+++ Maze.cpp
@@ -7,7 +7,7 @@
 #include <SFML/System/String.hpp>
 #include <stack>
 #include <random>
-
+#include <math.h>
 
 
 Maze::Maze(int mazeSize) {
@@ -17,7 +17,7 @@ Maze::Maze(int mazeSize) {
     this->playerPosition;
     this->rightMargin = 200;
     this->bottomMargin = 50;
-    this->mazeDimension = 0;
+    this->mazeDimension = 2;
 
     this->windowWidth = this->mazeSize * (this->cellSize + this->wallSize) + this->wallSize + this->rightMargin;
     this->windowHeight = this->mazeSize * (this->cellSize + this->wallSize) + this->wallSize + this->bottomMargin;
@@ -27,6 +27,9 @@ void Maze::run() {
     this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "Maze", sf::Style::Close);
     this->window.clear(sf::Color::White);
 
+    int tempDimension = this->mazeDimension;
+    int tempMazeSize = this->mazeSize;
+
     // text creation
     sf::Font font;
     font.loadFromFile("ARIAL.TTF");
@@ -34,38 +37,12 @@ void Maze::run() {
     {
         std::cout << "Font loading error";
     }
-
     sf::Text text("", font);
-    text.setString(
-        "Welcome to Ray's maze\n"
-        "To play select the number of dimensions for your maze \n"
-        "(2, 3, 4)"
-        /*"\n\n\n\n\n"
-        "Set the maze up:   Dimensions      Size\n\n"
-        "\t\t\t\t\t\t\t\t\t\t+  \t           +"
-        */
-    );
 
     text.setPosition(0, 0);
     text.setFillColor(sf::Color::Black);
 
-    /*
-    // button creation
-    sf::RectangleShape plusButton1(sf::Vector2f(50, 50));
-    plusButton1.setOutlineColor(sf::Color::Black);
-    plusButton1.setOutlineThickness(2);
-    plusButton1.setPosition(304, 311);
-
-    sf::RectangleShape plusButton2(sf::Vector2f(50, 50));
-    plusButton2.setOutlineColor(sf::Color::Black);
-    plusButton2.setOutlineThickness(2);
-    plusButton2.setPosition(458, 311);
-    */
-
-
-
     while (window.isOpen()) {
-
         sf::Event e;
         while (window.pollEvent(e)) {
             switch (e.type) {
@@ -74,45 +51,82 @@ void Maze::run() {
                 break;
 
             case sf::Event::TextEntered:
-                if ((char)e.text.unicode == '2') {
-                    this->window.close();
-                    Maze2D maze2D(this->mazeSize);
-                    maze2D.run();
-                    this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "2D Maze", sf::Style::Close);
+                if ((char)e.text.unicode == 'w') {
+                    tempMazeSize++;
                 }
-                else if ((char)e.text.unicode == '3') {
-                    this->window.close();
-                    Maze3D maze3D(this->mazeSize);
-                    maze3D.run();
-                    this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "2D Maze", sf::Style::Close);
+                else if ((char)e.text.unicode == 's' && tempMazeSize > 1) {
+                    tempMazeSize--;
                 }
-                else if ((char)e.text.unicode == '4') {
-                    this->window.close();
-                    Maze4D maze4D(this->mazeSize);
-                    maze4D.run();
-                    this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "2D Maze", sf::Style::Close);
+                else if ((char)e.text.unicode == 'i' && tempDimension < 4) {
+                    tempDimension++;
                 }
-
+                else if ((char)e.text.unicode == 'k' && tempDimension > 2) {
+                    tempDimension--;
+                }
                 break;
 
-
+            case sf::Event::KeyPressed:
+                if (e.key.code == sf::Keyboard::Enter) {
+                    this->window.close();
+                    this->mazeSize = tempMazeSize;
+                    if (tempDimension == 2) {
+                        Maze2D maze2D(this->mazeSize);
+                        maze2D.run();
+                        this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "2D Maze", sf::Style::Close);
+                    }
+                    else if(tempDimension == 3) {
+                        Maze3D maze3D(this->mazeSize);
+                        maze3D.run();
+                        this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "3D Maze", sf::Style::Close);
+                    }
+                    else if (tempDimension == 4) {
+                        Maze4D maze4D(this->mazeSize);
+                        maze4D.run();
+                        this->window.create(sf::VideoMode(1080, 720, sf::VideoMode::getDesktopMode().bitsPerPixel * 10), "4D Maze", sf::Style::Close);
+                    }
+                }
+                break;
             }
         }
 
+        std::string menu =
+            "Welcome to Ray's maze\n"
+            "Use w/s to set maze size and i/k to set maze dimension\n\n"
+            "Maze size: " + std::to_string(tempMazeSize) + "\n"
+            "Maze dimension: " + std::to_string(tempDimension) + "\n"
+            "Total number of cells: " + std::to_string((int)pow(tempMazeSize, tempDimension)) + "\n\n"
+            "Enter to start\n\n"
+            "Controls: \n";
 
+        switch (tempDimension) {
+            case 2:
+                menu +=
+                    "\tws - x axis"
+                    "\n\tad - y axis"
+                    "\n\t(standard maze - you know the controls)";
+                break;
 
+            case 3:
+                menu +=
+                    "\tws - x axis"
+                    "\n\tad - y axis"
+                    "\n\tik - z axis"
+                    "\n\t(getting harder but still playable)";
+                break;
+
+            case 4:
+                menu +=
+                    "\tws - x axis"
+                    "\n\tad - y axis"
+                    "\n\tik - z axis"
+                    "\n\tjl - w axis"
+                    "\n\t(w axis?!)";
+                break;
+        }
+        text.setString(menu);
 
         this->window.clear(sf::Color::White);
-
-       
-        
-        //this->window.draw(plusButton1);
-        //this->window.draw(plusButton2);
-        //this->window.draw(plusButton1);
-        //this->window.draw(plusButton1);
         this->window.draw(text);
-
-
         this->window.display();
     }
 }
--- Source.cpp
+++ Source.cpp
@@ -4,22 +4,7 @@
 #include "Maze2D.h"
 
 int main() {
-    std::string exit = "n";
-    while (exit == "n") {
-        int mazeSize = 0;
-        while (mazeSize < 1) {
-            std::cout << "Choose a maze size: ";
-            std::cin >> mazeSize;
-        }
-
-        Maze maze(mazeSize);
-        maze.run();
-
-        std::cout << "Do you want to exit? (y/n)";
-        std::cin >> exit;
-        system("cls");
-    }
-    
-  
+    Maze maze(5);
+    maze.run();
     return 0;
 }
